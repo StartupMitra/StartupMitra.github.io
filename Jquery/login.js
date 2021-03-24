4f13978999f972dea53fdafdc5f637923c96f633
StartupMitra <StartupMitra@outlook.com>
2021-03-24 16:17:30 +0530

    done

--- Jquery/login.js
+++ Jquery/login.js
@@ -2,29 +2,35 @@
     $(document).ready(function(){
 
         $('#submit').click(function(){
+          
+            username=$('#username').val()
+            password=$('#password').val()
+            user_data={"firstName":username,"lastName":password}
             
-    
+     
             var settings = {
-                "url": "http://127.0.0.1:5000/public",
+                "url": "http://127.0.0.1:5000/post",
                 "method": "POST",
                 "timeout": 0,
                 "headers": {
                 
                   "Content-Type": "application/json"
                 },
-                "data": JSON.stringify({"firstName":"hole222","lastName":"rushi"}),
+                "data": JSON.stringify(user_data),
               };
               
               $.ajax(settings).done(function (response) {
                 console.log(response);
                  
               });
-              alert("hey")   
-          
-    
-                 
-      
-    });
-        
-    });
+
+              alert("hey") 
+              alert(settings.data)  //returned data from server  settig.data
+             
+              
+              
+
+              });
+
+        });
   
