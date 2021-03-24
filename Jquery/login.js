a0dde4e82d2adcbc970930b955a9409d4a3ef6f1
StartupMitra <StartupMitra@outlook.com>
2021-03-24 15:00:09 +0530

    d

--- Jquery/login.js
+++ Jquery/login.js
@@ -1,26 +1,30 @@
-$(document).ready(function(){
 
-    $('#submit').click(function(){
-        
+    $(document).ready(function(){
 
-        var settings = {
-            "url": "127.0.0.1:5000/public",
-            "method": "POST",
-            "timeout": 0,
-            "headers": {
-            "Authorization": "Bearer dfdf",
-              "Content-Type": "application/json"
-            },
-            "data": JSON.stringify({"firstName":"hole222","lastName":"rushi"}),
-          };
-          
-          $.ajax(settings).done(function (response) {
-            console.log(response);
-          });
-      
-
-          alert("hey")        
-  
-});
+        $('#submit').click(function(){
+            
     
-});
+            var settings = {
+                "url": "http://127.0.0.1:5000/public",
+                "method": "POST",
+                "timeout": 0,
+                "headers": {
+                
+                  "Content-Type": "application/json"
+                },
+                "data": JSON.stringify({"firstName":"hole222","lastName":"rushi"}),
+              };
+              
+              $.ajax(settings).done(function (response) {
+                console.log(response);
+                 
+              });
+              alert("hey")   
+          
+    
+                 
+      
+    });
+        
+    });
+  
