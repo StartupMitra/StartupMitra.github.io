c9b1c691d44bd1e4f983a22c2e8167b143d49c98
StartupMitra <StartupMitra@outlook.com>
2021-05-31 18:09:21 +0530

    done

--- Jquery/cards.js
+++ Jquery/cards.js
@@ -15,10 +15,11 @@ $(document).ready(function(){
       country=response.data[i].country
       domain=response.data[i].domain
       short_summary=response.data[i].short_summary
-      logo=response.data[i].logo
+      logo=response.data[i].logo.split("\\").pop()
+      alert(logo)
       banner=response.data[i].banner
       
-      $( "h2" ).append('<div class="card"><img src="C:/Users/Yogesh/Pictures/f1.png" alt="John" style="width:100%">'+title+'</h1><p class="title">'+short_summary+'</p> <p>Harvard University</p>  <div style="margin: 24px 0;"><a href="#"><i class="fa fa-dribbble"></i></a> <a href="#"><i class="fa fa-twitter"></i></a>  <a href="#"><i class="fa fa-linkedin"></i></a>  <a href="#"><i class="fa fa-facebook"></i></a> </div><p><a href="file:///F:/StartupMitra/StartupMitra.github/profile/'+title+'">View</a></p></div>');  
+      $( "h2" ).append('<div class="card"><img src="F:/StartupMitra/data/'+logo+'" alt="John" style="width:100%">'+title+'</h1><p class="title">'+short_summary+'</p> <p>Harvard University</p>  <div style="margin: 24px 0;"><a href="#"><i class="fa fa-dribbble"></i></a> <a href="#"><i class="fa fa-twitter"></i></a>  <a href="#"><i class="fa fa-linkedin"></i></a>  <a href="#"><i class="fa fa-facebook"></i></a> </div><p><a href="file:///F:/StartupMitra/StartupMitra.github/profile/'+title+'">View</a></p></div>');  
     }
 
     });
