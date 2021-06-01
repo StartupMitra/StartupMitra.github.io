ed044bbc5b37166e6cf47422488ed4311611c210
StartupMitra <StartupMitra@outlook.com>
2021-06-01 15:41:14 +0530

    f

--- Jquery/profile.js
+++ Jquery/profile.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
     {
         var vars=[],hash;
         var hashes=window.location.href.slice(window.location.href.indexOf('?')+1).split('&');
+        $('#cancel').hide()
         for(var i=0;i<hashes.length;i++)
         {
             hash=hashes[i].split('=');
@@ -12,10 +13,11 @@ $(document).ready(function(){
         }
         return vars;
     }
-
+    var title=session_title=sessionStorage.getItem("title")
     var profile_name=getUrl()['profile']
     data={"profile_name":profile_name}
     alert(profile_name)
+    alert(title)
 
     var settings = {
         "url": "http://127.0.0.1:5000/entreprenuer_profile",
@@ -32,7 +34,36 @@ $(document).ready(function(){
       $.ajax(settings).done(function (response) {
     
        });
-    
+
+       $('#request').click(function(){
+           alert("hey")
+           data={"title":profile_name,"request":title}
+           var settings = {
+            "url": "http://127.0.0.1:5000/send_request",
+            "method": "POST",
+            "timeout": 0,
+            "headers": {
+            
+              "Content-Type": "application/json"
+            },
+            "data": JSON.stringify(data),
+          };
+          
+          
+          $.ajax(settings).done(function (response) {
+              if(response['data']=="1"){
+                  $('#request').hide()
+                  $('#cancel').show()
+              }
+              else{
+                $('#request').val("request")
+                $('#cancel').hide()
+              }
+        
+           });
+
+
+    });
       });
    
     
